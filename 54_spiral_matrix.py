e60a7e784a3c36a810d0ff9c5dc626452d7861d9
Alcides Ribeiro <alcidesribeirosampaioneto@gmail.com>
2023-06-07 11:03:15 -0300

    54 spiral matrx: improving

--- 54_spiral_matrix.py
+++ 54_spiral_matrix.py
@@ -1,7 +1,7 @@
 from typing import List
 
 class Solution:
-    def spiral_aux(self, i, j, p, q, matrix):
+    def spiral_aux(self, i, j, p, q):
         """ i => index of first line of matrix
             j => index of first column of matrix
             p => index of last line of matrix
@@ -14,25 +14,26 @@ class Solution:
         
         # case of single row
         if i == p:
-            return matrix[i][j:q+1]
+            return self.matrix[i][j:q+1]
 
         # case of single column
         if j == q:
-            return [ row[j] for row in matrix[i:p+1] ]
+            return [ row[j] for row in self.matrix[i:p+1] ]
         
         return (
-            matrix[i][j:q+1] +
-            [ row[q] for row in matrix[i+1:p] ] +
-            list(reversed(matrix[p][j:q+1])) +
-            list(reversed([ row[j] for row in matrix[i+1:p] ])) +
-            self.spiral_aux(i+1,j+1, p-1, q-1, matrix)
+            self.matrix[i][j:q+1] +
+            [ row[q] for row in self.matrix[i+1:p] ] +
+            list(reversed(self.matrix[p][j:q+1])) +
+            list(reversed([ row[j] for row in self.matrix[i+1:p] ])) +
+            self.spiral_aux(i+1,j+1, p-1, q-1)
         )
 
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
         M = len(matrix)
         N = len(matrix[0])
+        self.matrix = matrix
 
-        return self.spiral_aux(0, 0, M-1, N-1, matrix)
+        return self.spiral_aux(0, 0, M-1, N-1)
     
 
 m1 = [[1,2,3],[4,5,6],[7,8,9]]
@@ -40,5 +41,6 @@ m2 = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
 m3 = [[1,2],[3,4]]
 
 s = Solution()
-
+print(s.spiralOrder(m1))
+print(s.spiralOrder(m2))
 print(s.spiralOrder(m3))
