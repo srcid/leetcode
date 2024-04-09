64bcd73e567cd597c4385ddad91bb2bfdbd03275
Alcides Ribeiro <alcidesribeirosampaioneto@gmail.com>
2024-04-09 12:02:35 -0300

    quicksort and partition algorithms impl added

---
+++ sorting/qsort.py
@@ -0,0 +1,63 @@
+from rich import print
+from random import seed, randint
+from queue import Queue
+
+
+def partition(arr, start, end):
+    pivot_idx = start
+    pivot = arr[start]
+
+    for i in range(start+1, end):
+        if arr[i] <= pivot:
+            pivot_idx += 1
+            arr[i], arr[pivot_idx] = arr[pivot_idx], arr[i]
+
+    arr[pivot_idx], arr[start] = arr[start], arr[pivot_idx]
+
+    return pivot_idx
+
+
+def qsort(arr, start, end):
+    if end - start >= 2:
+        mid = partition(arr, start, end)
+
+        qsort(arr, start, mid)
+        qsort(arr, mid + 1, end)
+
+
+def iqsort(arr):
+    q = Queue()
+    q.put((0, len(arr)))
+
+    while not q.empty():
+        start, end = q.get()
+
+        if end - start >= 2:
+            mid = partition(arr, start, end)
+            q.put((start, mid))
+            q.put((mid + 1, end))
+
+
+seed(42)
+
+arr = [randint(-19, 20) for _ in range(20)]
+p = [*filter(lambda n: n <= arr[0], arr[1:]), arr[0], *filter(lambda n: n > arr[0], arr[1:])]
+p_idx = partition(arr, 0, len(arr))
+
+print('Partition algorithm')
+print(f'{'initial:':12} {arr}')
+print(f'{'pivot_idx:':12} {p_idx}')
+print(f'{'partitioned:':12} {arr}')
+print(f'{'comp:':12} {p[p_idx] == arr[p_idx]}')
+
+print('QSort algorithm')
+
+arr = [randint(-1000, 1000) for _ in range(100_000)]
+arr_cp = arr.copy()
+
+qsort(arr_cp, 0, len(arr_cp))
+print(f'{'qsort:':12} {arr_cp == sorted(arr)}')
+
+arr_cp = arr
+iqsort(arr_cp)
+print(f'{'iqsort:':12} {arr_cp == sorted(arr)}')
