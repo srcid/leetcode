6961f8e765764a8f74e01bc2b0c2a37c6d2d201f
Alcides Ribeiro <alcidesribeirosampaioneto@gmail.com>
2024-04-10 10:08:52 -0300

    impl of func that returns indexes of an nth dimension array

---
+++ random/indexes.py
@@ -0,0 +1,49 @@
+from typing import Iterable, Union
+from itertools import product
+
+def indexes(arr: Iterable) -> Union[tuple[int], tuple[int]]:
+    """Returns the indexes of elements in a N-th dimensional array
+
+    Args:
+        arr (Iterable): N-th dimensional array 
+
+    Returns:
+        Union[tuple[int], tuple[int]]: tuple of indexes if arr is 1-th dimension, tuple with tuple
+        of index otherwise.
+    """
+
+    subs = []
+    
+    while isinstance(arr[0], Iterable):
+        subs.append(range(len(arr)))
+        arr = arr[0]
+
+    return tuple(product(*subs, range(len(arr)))) if subs else tuple(range(len(arr)))
+
+
+if __name__ == '__main__':
+    arr_1d = [1,3,3]
+
+    arr_2d = [
+        [1,2,3],
+        [4,5,6],
+        [7,8,9]
+    ]
+
+    arr_3d = [
+    [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ],
+    [
+        [10, 11, 12],
+        [13, 14, 15],
+        [16, 17, 18]
+    ]
+]
+
+
+    print(indexes(arr_1d))
+    print(indexes(arr_2d))
+    print(indexes(arr_3d))
