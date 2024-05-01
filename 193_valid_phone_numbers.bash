338561c4f90294c677bae43332e520a10c63a565
Alcides Ribeiro <alcidesribeirosampaioneto@gmail.com>
2024-04-30 22:14:06 -0300

    193 valid phone numbers added sols

---
+++ 193_valid_phone_numbers.bash
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+# Read from the file file.txt and output all valid phone numbers to stdout.
+
+cat file.txt | while read -r phone; do if [[ $phone =~ ^([[:digit:]]{3}-[[:digit:]]{3}-[[:digit:]]{4}|\([[:digit:]]{3}\) [[:digit:]]{3}-[[:digit:]]{4})$ ]]; then echo $phone; fi done
+
+while read -r phone; do if [[ $phone =~ ^([[:digit:]]{3}-[[:digit:]]{3}-[[:digit:]]{4}|\([[:digit:]]{3}\) [[:digit:]]{3}-[[:digit:]]{4})$ ]]; then echo $phone; fi done < file.txt
+
+grep -oE '^([[:digit:]]{3}-[[:digit:]]{3}-[[:digit:]]{4}|\([[:digit:]]{3}\) [[:digit:]]{3}-[[:digit:]]{4})$' file.txt
+
+grep -oP '^(\d{3}-|\(\d{3}\) )\d{3}-\d{4}$' file.txt
+
+awk '$0 ~ /^([[:digit:]]{3}-|\([[:digit:]]{3}\) )[[:digit:]]{3}-[[:digit:]]{4}$/ { print $0 }' file.txt
