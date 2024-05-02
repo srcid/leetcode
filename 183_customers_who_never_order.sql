a05706a60fc7df5c3d775a322b2d02866b68f8f7
Alcides Ribeiro <alcidesribeirosampaioneto@gmail.com>
2024-05-02 00:35:19 -0300

    183 customers who never order: added sols

---
+++ 183_customers_who_never_order.sql
@@ -0,0 +1,15 @@
+# Write your MySQL query statement below
+
+select name as Customers
+from customers
+where
+    id not in(
+        select customerId
+        from orders
+    );
+
+select name as Customers
+from customers c
+    left join orders o on c.id = o.customerId
+where
+    o.customerId is null;
