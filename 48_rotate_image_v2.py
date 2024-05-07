b367e9cedd8501e9e298bd251cffb0c4ae3e2dff
Alcides Ribeiro <alcidesribeirosampaioneto@gmail.com>
2024-05-06 22:13:43 -0300

    48 rotate image v2: added more test case

--- 48_rotate_image_v2.py
+++ 48_rotate_image_v2.py
@@ -13,6 +13,15 @@ class Solution:
 
 if __name__ == "__main__":
     matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    matrix2 = [
+        [1,   2,  3,  4,  5],
+        [6,   7,  8,  9, 10],
+        [11, 12, 13, 14, 15],
+        [16, 17, 18, 19, 20],
+        [21, 22, 23, 24, 25],
+    ]
     s = Solution()
     s.rotate(matrix)
     print(matrix)
+    s.rotate(matrix2)
+    print(matrix2)
