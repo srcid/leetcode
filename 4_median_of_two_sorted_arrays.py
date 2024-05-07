c5cdb4f55a1057cc8df33d5c3a86f0e4e280aad0
Alcides Ribeiro <alcidesribeirosampaioneto@gmail.com>
2024-05-07 15:19:54 -0300

    4 median of two sorted arrays: fixed sol

--- 4_median_of_two_sorted_arrays.py
+++ 4_median_of_two_sorted_arrays.py
@@ -1,58 +1,71 @@
 from rich import print
 from typing import List
-from math import inf
+from math import ceil, floor, inf
 
 
 class Solution:
+    @staticmethod
     def median(nums):
-        mid = len(nums) // 2
-        
-        if len(nums) % 2:
-            return (nums[mid] + nums[mid+1]) / 2
-        
-        return nums[mid]
+        mid = (len(nums) - 1) / 2
+        l, r = floor(mid), ceil(mid)
+        return (nums[l] + nums[r]) / 2
+
+    @staticmethod
+    def getitem(arr, idx, defaut):
+        if 0 <= idx < len(arr):
+            return arr[idx]
+        else:
+            return defaut
 
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
         M, N = len(nums1), len(nums2)
-        MERGED_SIZE = M + N  # Size of arrays merged
-        MERGED_HALF = MERGED_SIZE // 2
-        
+        TOTAL = M + N
+        HALF = (TOTAL - 1) // 2
+
         if N < M:
             nums1, nums2 = nums2, nums1
             M, N = N, M
 
         if M == 0:
             return self.median(nums2)
-        
-        l, r = 0, M - 1
+
+        start, end = 0, M - 1
 
         while True:
-            i = (l+r) // 2
-            j = MERGED_HALF - i - 2
+            mid1 = (start + end) // 2
+            mid2 = HALF - mid1 - 1
 
-            nums1Left  = nums1[i]   if i >= 0    else -inf
-            nums1Right = nums1[i+1] if (i+1) < M else inf
-            nums2left  = nums2[j]   if j >= 0    else -inf
-            nums2Right = nums2[j+1] if (j+1)     else inf
+            print(f"{mid1=} {mid2=}")
 
-            # Partition is correct
-            if nums1Left <= nums2Right and nums2left <= nums1Right:
-                # Odd
-                if MERGED_SIZE % 2:
-                    return min(nums1Right, nums2Right)
-                # Even
-                else:
-                    return (max(nums1Left, nums2left) + min(nums1Right, nums2Right)) / 2
-            elif nums1Left > nums2Right:
-                r = i - 1
+            l1 = self.getitem(nums1, mid1, -inf)
+            r1 = self.getitem(nums1, mid1 + 1, inf)
+            l2 = self.getitem(nums2, mid2, -inf)
+            r2 = self.getitem(nums2, mid2 + 1, inf)
+
+            print(f"{l1=} {r1=} {l2=} {r2=}")
+
+            if l1 <= r2 and l2 <= r1:
+                if TOTAL % 2 == 0:
+                    return (max(l1, l2) + min(r1, r2)) / 2
+
+                return max(l1, l2)
+            elif l1 > r2:
+                end = mid1 - 1
             else:
-                l = i + 1
+                start = mid1 + 1
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     sol = Solution()
-    
-    # nums1, nums2 = [0,2,6], [0,7,16,17,19]
-    # print(sol.findMedianSortedArrays(nums1, nums2))
-    
+
+    print("teste 1: ")
+    nums1, nums2 = [0, 2, 6], [0, 7, 16, 17, 19]
+    print(sol.findMedianSortedArrays(nums1, nums2), end="\n\n")
+
+    print("teste 2: ")
     nums1, nums2 = [100001], [100000]
+    print(sol.findMedianSortedArrays(nums1, nums2), end="\n\n")
+
+    print("teste 3: ")
+    nums1, nums2 = [0, 2, 6, 7], [0, 7, 16, 17, 19]
     print(sol.findMedianSortedArrays(nums1, nums2))
