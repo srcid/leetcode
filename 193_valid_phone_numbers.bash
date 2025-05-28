4ba7f3fd29576756e867e0d075ee771557912552
Alcides Ribeiro <alcidesribeirosampaioneto@gmail.com>
2025-05-27 21:11:13 -0300

    leetcode: update 193 with file content sample

--- 193_valid_phone_numbers.bash
+++ 193_valid_phone_numbers.bash
@@ -1,6 +1,11 @@
 #!/usr/bin/bash
 # Read from the file file.txt and output all valid phone numbers to stdout.
 
+## File content sample
+# 987-123-4567
+# 123 456 7890
+# (123) 456-7890
+
 cat file.txt | while read -r phone; do if [[ $phone =~ ^([[:digit:]]{3}-[[:digit:]]{3}-[[:digit:]]{4}|\([[:digit:]]{3}\) [[:digit:]]{3}-[[:digit:]]{4})$ ]]; then echo $phone; fi done
 
 while read -r phone; do if [[ $phone =~ ^([[:digit:]]{3}-[[:digit:]]{3}-[[:digit:]]{4}|\([[:digit:]]{3}\) [[:digit:]]{3}-[[:digit:]]{4})$ ]]; then echo $phone; fi done < file.txt
